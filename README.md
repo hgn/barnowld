3723b5434b39c9485669e696deadd37a76eb524d
Hagen Paul Pfeifer <hagen@jauu.net>
2023-09-22 11:59:15 +0200

    updates: new features
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BarnOwlD - A Cache Side-Channel Real Time Attack Detector
+# BarnOwlD - A Daemon for Real-Time Detection of Cache Side-Channel Attacks
 
 **The name is inspired by a barn owl, which has one of the best hearing in the
 animal world. Just the thing to detect microscopic but massive cache misses and
@@ -10,6 +10,25 @@ computational effort as possible, which can be traced back to side-channel
 attacks with a very high probability. If these are detected, report the whole
 thing to the system log. Log monitoring systems for cloud systems, for example,
 can trigger a warning when these messages are detected.
+
+# Implementation Aspects
+
+Barnowld analyzes all available logical CPUs (hardware threads, called Harts in
+the RISC-V world) for abnormalities over a certain period of time. In doing so,
+it iterates over time and in a pseudo-random fashion over the CPUs to make
+countermeasures more difficult. Then Barnowl analyzes the cache reference and
+cache missrate for a certain amount of time - again pseudo-randomly. Here
+especially the last level cache charactetristics.
+
+For this purpose Barnwold uses the so-called Counting Mode of the Performance
+Monitor Unit (PMU) of the processor. In contrast to the Sampling Mode, the
+Counting Mode has no measurable overhead.
+
+The whole thing is based on the perf subsystem of the Linux kernel which is
+designed around two aspects: flexibility and performance. In fact, perf can be
+seen as a command system call, which efficiently exchanges data between kernel
+space and user space using a ring buffer. So ideal conditions if these analyses
+are to be made with lowest overhead.
 
 # PoC Test Disclaimer
 
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,105 @@
+extern crate clap;
+use clap::{Arg, Command};
+
+use libc;
 use perf_event::events::Hardware;
 use perf_event::Builder;
 use std::thread;
 use std::time::Duration;
+
+pub struct Config {
+    verbose: bool,
+}
+
+impl Config {
+    pub fn new() -> Self {
+        Config { verbose: false }
+    }
+
+    pub fn verbose(&self) -> bool {
+        self.verbose
+    }
+}
+
+fn get_num_cpus() -> usize {
+    const CONF_NAME: libc::c_int = libc::_SC_NPROCESSORS_ONLN;
+
+    let cpus = unsafe { libc::sysconf(CONF_NAME) };
+    if cpus < 1 {
+        1
+    } else {
+        cpus as usize
+    }
+}
 
 fn xsleep(sleeptime: u64) {
     let sleep_duration = Duration::from_secs(sleeptime);
     thread::sleep(sleep_duration);
 }
 
-fn main() -> std::io::Result<()> {
+fn cli() -> Command {
+    Command::new("barnowld")
+        .version("0.1.0")
+        .author("Hagen Paul Pfeifer <hagen@jauu.net>")
+        .about("A Daemon for Real-Time Detection of Cache Side-Channel Attacks")
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .required(false)
+                .num_args(0)
+                .help("Increase verbosity level"),
+        )
+}
+
+fn parse_args() -> Config {
+    let mut cfg = Config::new();
+    let matches = cli().get_matches();
+
+    if let Some(c) = matches.get_one::<bool>("verbose") {
+        cfg.verbose = *c;
+    }
+
+    return cfg;
+}
+
+fn recording(cpu: usize, cfg: Config) {
     let mut cache_refs = Builder::new()
-        .one_cpu(0)
+        .one_cpu(cpu)
         .observe_pid(-1)
         .kind(Hardware::CACHE_REFERENCES)
-        .build()?;
+        .build().unwrap();
 
     let mut cache_misses = Builder::new()
-        .one_cpu(0)
+        .one_cpu(cpu)
         .observe_pid(-1)
         .kind(Hardware::CACHE_MISSES)
-        .build()?;
+        .build().unwrap();
 
-    cache_refs.enable()?;
-    cache_misses.enable()?;
+    cache_refs.enable().unwrap();
+    cache_misses.enable().unwrap();
     xsleep(2);
-    cache_refs.disable()?;
-    cache_misses.disable()?;
+    cache_refs.disable().unwrap();
+    cache_misses.disable().unwrap();
 
-    let cache_misses_no = cache_misses.read()?;
-    let cache_refs_no = cache_refs.read()?;
+    let cache_misses_no = cache_misses.read().unwrap();
+    let cache_refs_no = cache_refs.read().unwrap();
     let ratio = (cache_misses_no as f64 / cache_refs_no as f64) * 100.0;
 
-    println!(
-        "misses: {} refs: {}, ratio: {}",
-        cache_misses_no, cache_refs_no, ratio
-    );
+    if cfg.verbose {
+        println!(
+            "misses: {} refs: {}, ratio: {}",
+            cache_misses_no, cache_refs_no, ratio
+        );
+    }
+
+}
+
+fn main() -> std::io::Result<()> {
+    let cfg = parse_args();
+    let no_cpus = get_num_cpus();
+
+    recording(0, cfg);
 
     Ok(())
 }
